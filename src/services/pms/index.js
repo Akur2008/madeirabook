6bd74ff69a011f013a2d809017ec7bdcbd0db148
Akur2008 <akur2008@yandex.ru>
2026-09-13 15:30:16 +0100

    Create index.js

---
+++ src/services/pms/index.js
@@ -0,0 +1,7 @@
+import * as smoobu from './smoobu.js';
+
+// Точка абстракции над PMS.
+// Сейчас используем Smoobu. Если в будущем перейдем на Zeevou,
+// просто поменяем импорт здесь, не трогая остальной код.
+
+export const pms = smoobu;
