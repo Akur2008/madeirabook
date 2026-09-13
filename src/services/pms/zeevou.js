ef38828b149be1513e5d350d47ee47bb2a49b9af
Akur2008 <akur2008@yandex.ru>
2026-09-13 15:33:48 +0100

    Create zeevou.js

---
+++ src/services/pms/zeevou.js
@@ -0,0 +1,7 @@
+// ЗАГОТОВКА ПОД ZEEVOU.
+// Не используется в текущей версии.
+// Служит напоминанием, что абстракция PMS существует.
+
+export async function getApartments() {
+  throw new Error('Zeevou integration not implemented yet');
+}
