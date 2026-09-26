b172356605b5d373a46421ed0a8f7ad6237f15cb
Andrei Kurochkin <akur2013@gmail.com>
2026-09-26 21:48:14 +0100

    fix(core): allow X-Telegram-Init-Data header in CORS

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use(
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, X-Telegram-Init-Data');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
